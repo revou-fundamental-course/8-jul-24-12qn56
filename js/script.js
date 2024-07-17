7a1914e6a10d803a5549ad11bc0863fc19a69e6c
12qn56 <kabukijkpro@gmail.com>
2024-07-17 19:55:05 +0700

    message

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-//ini javascript
+// Fungsi Kalkulasi BMI
+function calculate(){
+var number1 = parseInt(document.getElementById("input-berat-badan").value);
+var number2 = parseInt(document.getElementById("input-tinggi-badan").value);
+let cmToMeter;
+let sqHeight;
+let calcResult;
+
+// jika null maka akan muncul NaN
+if (number1 != null && number2 != null){
+  cmToMeter = number2 / 100;
+  sqHeight = cmToMeter * cmToMeter;
+  calcResult = number1 / sqHeight;
+  document.getElementById("calcResultH2").innerHTML = calcResult;
+}
+}
